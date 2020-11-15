24ce9d551f3fe2607f413f80d8c3fd04d760478b
Naveen Singh <naveensing@vidsysoft.com>
2020-11-16 00:11:29 +0530

    Nov 15 Day 2 Inheritance and Method Overriding

---
+++ oops_nov15.js
@@ -0,0 +1,54 @@
+// 1. Inheritance - Acquring the properties from one class to another class
+// Using Keywords extends
+// Types of Inheritance 
+// A->B = Single Inheritance 
+// A->B->C = Multiple Level Inheritance
+//  A
+//  |
+//  /\
+// B  C = Hierarchial Inheritance
+
+class A {
+    a = 100;
+    display(){
+        console.log(this.a);
+    }
+}
+
+class B extends A{
+    b = 200;
+    show(){
+        console.log(this.b);
+    }
+}
+
+bobj = new B();
+bobj.display();
+bobj.show();
+
+// Method Overriding 
+
+class Bank {
+    roi(){
+        return 0;
+    }
+}
+
+
+
+class AXIS extends Bank {
+    roi(){
+        return 10.5;
+    }
+}
+
+class SBI extends Bank {
+    roi(){
+        return 12.7;
+    }
+}
+
+sbi = new SBI();
+console.log('SBI',sbi.roi());
+ax = new AXIS();
+console.log('AXIS',ax.roi());
