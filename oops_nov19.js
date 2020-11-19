f87032470e95cbfb660e5b6e4c79fefcd14785be
naveensinghp <naveensingh.mtp@gmail.com>
2020-11-20 01:17:17 +0530

    “Day6”

---
+++ oops_nov19.js
@@ -0,0 +1,18 @@
+// Static Keyword in JS
+
+class Test {
+    static a =10; // Static Variable
+    b = 100;
+
+    static m1(){
+        console.log("This is Static Method");
+    }
+    m2(){
+        console.log("This is Non Static Method");
+    }
+}
+// 1. We can directly acess static variable and methods using class name
+console.log(Test.a);
+Test.m2();
+
+// We can access non static variables & Method using Object
