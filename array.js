f0ef6ab8dfd5e59277e49b94c2daae6c30ae7ead
naveensinghp <naveensingh.mtp@gmail.com>
2020-11-27 08:30:24 +0530

    “Day13”

--- array.js
+++ array.js
@@ -1,57 +1,18 @@
 
 // Call Stack
 
-function multiply(a,b){
-    return a * b;
-}
-
-function square(n){
-    return multiply(n,n);
-}
-
-function printSquare(n){
-    var squared = square(n);
-    console.log(squared);
-}
-
-printSquare(4);
 
 
-let name = ["naveen", 63,['bike','car']];
-console.log(name);
+const number = ['a','b','b','c','c','d','e','f'];
 
-let sequence = [1,2,3,4,5,6];
-for(let i=0; i<sequence.length; i++){
-    console.log(sequence[i]);
-}
+let count = {};
 
-const cats = ['Bill', 'Jeff', 'Pete', 'Biggles', 'Jasmin'];
-let info = 'My cats are called ';
-const para = document.querySelector('p');
-
-for (let i = 0; i < cats.length; i++) {
-  info += cats[i] + ', ';
-}
-
-para.textContent = info;
-
-
-const contacts = ['Chris:232323','Sarah:345335','Bil:768689','Mary:94848'];
-const paras = document.querySelector('p');
-const input = document.querySelector('input');
-const btn = document.querySelector('button');
-
-btn.addEventListener('click',function(){
-    let searchName = input.value.toLocaleLowerCase();
-    input.value = '';
-    // input.focus();
-    for(let i=0; i < contacts.length; i++){
-        let splitContact = contacts[i].split(':');
-        if(splitContact[0].toLocaleLowerCase() === searchName){
-            para.textContent = splitContact[0] + '\'s number is ' + splitContact[1] + '.';
-            break;
-        }else{
-            paras.textContent = 'Contact Not Found';
-        }
+number.forEach(item => {
+    if(count[item]){
+        count[item]++;
+    }else{
+        count[item] =1;
     }
 });
+
+console.log(count);
