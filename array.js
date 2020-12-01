de97bb7b9c3bf7954175841c9bd7903dd02603d3
naveensinghp <naveensingh.mtp@gmail.com>
2020-12-02 00:41:04 +0530

    “Day12”

--- array.js
+++ array.js
@@ -16,3 +16,28 @@ number.forEach(item => {
 });
 
 console.log(count);
+
+function multiply(arr,n){
+
+    var product = 1;
+    for(var i =0; i < n; i++){
+        product *= arr[i];
+    }
+    return product;
+
+
+}
+
+// Nesting for loops
+
+function multiplyAll(arr){
+    var product = 1;
+    for(var i =0; i < arr.length; i++){
+        for(var j=0; j<arr[i].length; j++){
+            product = product * arr[i];
+        }
+    }
+
+    return product;
+}
+multiplyAll([1,2],[3,4],[5,6,7]);
