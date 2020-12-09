01a9756157b49c1aece6d93edb5c017e511e21d3
naveensinghp <naveensingh.mtp@gmail.com>
2020-12-09 23:55:09 +0530

    “Day18”

--- 30days.js
+++ 30days.js
@@ -1,12 +1,13 @@
 
-// Day 1: 
+// Practising #CodeTidbits30 
+// Day 6: 
 
 //   <audio controls="true" autoplay="false" muted="true">
 //     <source src="test.mp3" type="audio/mpeg" />
 //     Your browser does not support the audio tag.
 //   </audio> 
 
-// Day 2:  4 Ways to Convert String to Character Array in JavaScript
+// Day 7:  4 Ways to Convert String to Character Array in JavaScript
 // Spliting the string 
 
 const string = 'Hello World';
@@ -15,6 +16,13 @@ console.log('Array.form(string)',Array.from(string));
 console.log('Object.assing([],string)',Object.assign([],string));
 
 
-// Day 3: Math.max()
+// Day 8: Math.max()
 const array = [1,3,80];
 console.log('max',Math.array());
+
+// Day 9 Better Boolean value 
+
+const isPerson = true;
+const hasAge = false
+const canDance = true
+
